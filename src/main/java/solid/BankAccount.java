cdf62fd12997394ab3d30058493ef19e204d95ee
supanit.sup@ngernturbo.com <supanit.sup@ngernturbo.com>
2021-08-28 17:47:48 +0700

    my service

--- src/main/java/solid/BankAccount.java
+++ src/main/java/solid/BankAccount.java
@@ -4,12 +4,30 @@ class Main {
     public static void main(String[] args) {
         BankAccount bankAccount = new BankAccount();
         bankAccount.transferMoney(new BankAccount(),100);
+        BankAccount target = new InterBankAccount();
+        bankAccount.transferMoney(target, 100);
+
     }
 }
 
+enum AccountType {
+    Thailand,
+    Inernational
+}
+
 public class BankAccount {
+    AccountType type = AccountType.Thailand;
+
     void transferMoney(BankAccount targetAccount, int amount) {
         //tranfer moner process
         System.out.println("Transfer money in Thailand");
+    }
+}
+
+class InterBankAccount extends BankAccount {
+    @Override
+    void transferMoney(BankAccount targetAccount, int amount) {
+        //tranfer moner process
+        System.out.println("Transfer money in Inter");
     }
 }
---
+++ src/main/java/solid/MyServiceTest.java
@@ -0,0 +1,13 @@
+package solid;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyServiceTest {
+    @Test
+    void generateUUID() {
+        MyService myService = new MyService();
+        assertEquals("DEMO7", myService.generateUUID());
+    }
+}
