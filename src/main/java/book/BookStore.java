b982059b1eb328443180d2303fc39aa096b40ecd
supanit.sup@ngernturbo.com <supanit.sup@ngernturbo.com>
2021-08-28 15:09:01 +0700

    add file

---
+++ src/main/java/book/BookStore.java
@@ -0,0 +1,11 @@
+package book;
+
+public class BookStore {
+    public static void main(String[] args) {
+        BookStore bookStroe = new BookStore();
+        bookStroe.process();
+    }
+
+    private void process() {
+    }
+}
